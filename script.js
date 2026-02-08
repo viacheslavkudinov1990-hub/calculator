f7c401ae3f4a48511267b810df2920d8b13f1e44
viacheslavkudinov1990-hub <251155980+viacheslavkudinov1990-hub@users.noreply.github.com>
2026-02-08 14:11:02 +0100

    added files index, style, script. added first simple UI

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("script uploaded");
+const numbers = document.querySelector(".numbers");
+const input = document.querySelector(".output");
+input.value = "0";
+let expression = [];
+
+function addNoMoreThan3Item(arr, item) {
+    return 1
+}
+//numbers.addEventListener("click", )
