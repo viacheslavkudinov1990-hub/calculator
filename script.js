41ce19b58ff85bd597ad6d510e8514c342a3ae47
viacheslavkudinov1990-hub <251155980+viacheslavkudinov1990-hub@users.noreply.github.com>
2026-02-10 21:54:15 +0100

    added events for numbers and operators

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
 console.log("script uploaded");
 const numbers = document.querySelector(".numbers");
 const input = document.querySelector(".output");
-input.value = "0";
+const operators = document.querySelector(".operators");
+const buttonpoint = document.querySelector(".point")
+input.value = "";
 let expression = [];
+let allSigns = ["+", "-", "/", "*"];
+let inputNumber = '';
+
+function calculateTextExpression(exp) {
+    switch(exp[1]) {
+        case "+":
+            return +exp[0] + +exp[2];
+        case "-":
+            return +exp[0] - +exp[2];
+        case "*":
+            return +exp[0] * +exp[2];
+        case "/":
+            return +exp[0] / +exp[2];
+    }
+}
 
 function addNoMoreThan3Item(arr, item) {
-    return 1
+    if(arr.length == 3) {
+        let resultOftotalExpression = calculateTextExpression(arr);
+        arr.length = 0;
+        arr.push(resultOftotalExpression);
+        arr.push(item);
+    }else if(arr.length == 2 && allSigns.includes(item)) {
+        arr[1] = item;
+    }else{
+        arr.push(item);
+    }
 }
-//numbers.addEventListener("click", )
+numbers.addEventListener("click", (event) => {
+    let target = event.target;
+    if(target.className === "num") {
+        input.value += target.textContent;
+        inputNumber += target.textContent;
+    };
+    
+})
+operators.addEventListener("click", (event) => {
+    let target = event.target;
+    if(target.className === "operator") {
+        addNoMoreThan3Item(expression, inputNumber);
+        addNoMoreThan3Item(expression, target.textContent);
+        inputNumber = '';
+        input.value = expression.join('');
+    }
+})
+
